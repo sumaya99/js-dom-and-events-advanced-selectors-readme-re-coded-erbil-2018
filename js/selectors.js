ebf1e60e9435f19a84cf0729a4aaf54e3d79eafe
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-09 04:12:25 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+
+
+
+function lastImageSelector(){
+return $('img:last')}
+
+function ninjaBabySelector(){
+return $('#baby-ninja')
+}
+function divSelector(){
+return $('.pics:nth-child(1)') + $('.pics:nth-child(2)')}
+function firstListItem(){
+return $('#pics-list  li:first-child')}
+
+console.log(divSelector())
