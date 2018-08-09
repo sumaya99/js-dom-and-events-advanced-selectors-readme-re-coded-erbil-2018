9993e0c81c9686f2a366370967390a1b8688a45e
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-09 04:00:33 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,17 +1,2 @@
 // declare your functions here...
-$(document).ready(function paragraphSelector(
-  let target = $()
-  
-  ){})
 
-
-
-
-function lastImageSelector(){}
-
-
-function ninjaBabySelector(){}
-
-function divSelector(){}
-
-function firstListItem(){}
